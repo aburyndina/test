1ab1ffe369181ee8af2230c3d0d427a74fadca6e
aburyndina <aburyndina@servicetitan.com>
2015-12-20 19:48:58 +0500

    2 add

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             var i = 0;
-            Console.WriteLine("test");
+            Console.WriteLine("test 22");
         }
     }
 }
